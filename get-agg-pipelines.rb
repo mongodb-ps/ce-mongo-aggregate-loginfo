234344947b0fea2ce79fc62d7500db9595b98f35
Timo Geusch <timo.geusch@mongodb.com>
2019-11-28 13:37:42 -0800

    Removed parameters for $in clauses

--- get-agg-pipelines.rb
+++ get-agg-pipelines.rb
@@ -15,34 +15,28 @@ def match_square_brackets(str)
       r_i = i
       found_first = false
     end
-    #puts i
   end
   return str[0..r_i]
+end
+
+def remove_in_clauses(str)
+  return str.gsub(/\$in:\s+\[[^\[\]]*\]/,'$in: [ <removed> ]')
 end
 
 pipelines = {}
 
 ARGF.each do |line|
-  #match_group = line.scan(/(\{ aggregate: \"(.+)\", (pipeline: \[.+\]) \})/)
-  #matches = line.match(/(\{ aggregate: \"(.+)\",\s+(pipeline:\s+\[.+\])\s*\})/)
-  #matches = line.match(/(\{ aggregate: \"(.+)\",\s+pipeline:\s+(?<pipeline>\[[^\[\]]*\g<pipeline>*\])\s*\})/)
-  #matches = line.match(/(pipeline:\s+(?<pipeline>\[[^\[\]]*\g<pipeline>*\]))/)
   matches = line.match(/(\{\s+aggregate:\s+\"(.+)\",\s+(pipeline:\s+\[.*)protocol:op_msg (\d+)ms$)/)
   unless matches.nil?
     #puts match
     if matches.length > 0
       all, collection, pl, exec_time = matches.captures
-      #all, collection, pipeline = matches.captures
-      #puts collection
-      pipeline = match_square_brackets(pl)
-      #puts exec_time
+      pipeline = collection + "\t\t" + remove_in_clauses(match_square_brackets(pl))
 
       if not pipelines.key?(pipeline)
         pipelines[pipeline] = Array(exec_time)
       else
-        exec_times = pipelines[pipeline]
-        exec_times.push(exec_time)
-        pipelines[pipeline] = exec_times
+        pipelines[pipeline].push(exec_time)
       end
     end
   end
