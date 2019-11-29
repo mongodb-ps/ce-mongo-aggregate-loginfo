d8fae80f183446e4800ab918753b779316f2b87e
Timo Geusch <timo.geusch@mongodb.com>
2019-11-29 10:48:31 -0800

    Improved output formatting

--- get-agg-pipelines.rb
+++ get-agg-pipelines.rb
@@ -23,25 +23,33 @@ def remove_in_clauses(str)
   return str.gsub(/\$in:\s+\[[^\[\]]*\]/,'$in: [ <removed> ]')
 end
 
+def print_stats(pipeline, exec_times)
+  exec_times.sort!
+  min = exec_times[0]
+  max = exec_times[exec_times.size - 1]
+  avg = exec_times.inject(0.0) { | sum, val | sum + val } / exec_times.size
+
+  printf("%s\t\t\t%d\t%d\t%.2f\n", pipeline, min, max, avg)
+end
+
 pipelines = {}
 
 ARGF.each do |line|
   matches = line.match(/(\{\s+aggregate:\s+\"(.+)\",\s+(pipeline:\s+\[.*)protocol:op_msg (\d+)ms$)/)
   unless matches.nil?
-    #puts match
     if matches.length > 0
       all, collection, pl, exec_time = matches.captures
       pipeline = collection + "\t\t" + remove_in_clauses(match_square_brackets(pl))
 
       if not pipelines.key?(pipeline)
-        pipelines[pipeline] = Array(exec_time)
+        pipelines[pipeline] = Array(exec_time.to_f)
       else
-        pipelines[pipeline].push(exec_time)
+        pipelines[pipeline].push(exec_time.to_f)
       end
     end
   end
 end
 
-pipelines.each do |p|
-  puts p
+pipelines.each do |key, value|
+  print_stats(key, value)
 end
